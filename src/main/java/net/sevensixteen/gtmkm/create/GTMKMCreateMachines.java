0f36d5ff933c1fe70e1e5c81c6cbb64422666556
SevenSixteen <83654349+poggers16@users.noreply.github.com>
2023-06-23 22:42:44 +0700

    make init blank

--- src/main/java/net/sevensixteen/gtmkm/create/GTMKMCreateMachines.java
+++ src/main/java/net/sevensixteen/gtmkm/create/GTMKMCreateMachines.java
@@ -6,9 +6,9 @@ import com.gregtechceu.gtceu.common.machine.KineticMachineDefinition;
 import net.sevensixteen.gtmkm.recipe.GTMKMRecipeTypes;
 
 public class GTMKMCreateMachines {
-    public static KineticMachineDefinition[] KINETIC_CHEMICAL_REACTOR;
+    public static final KineticMachineDefinition[] KINETIC_CHEMICAL_REACTOR = GTCreateMachines.registerSimpleKineticElectricMachine("kinetic_chemical_reactor", GTMKMRecipeTypes.CREATE_CHEMICAL_RECIPES, GTMachines.LOW_TIERS);;
 
     public static void init() {
-        KINETIC_CHEMICAL_REACTOR = GTCreateMachines.registerSimpleKineticElectricMachine("kinetic_chemical_reactor", GTMKMRecipeTypes.CREATE_CHEMICAL_RECIPES, GTMachines.LOW_TIERS);
+
     }
 }
